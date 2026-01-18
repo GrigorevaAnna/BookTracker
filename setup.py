87e69c9cc0c49ece587327f25565a1d5cf980cbb
GrigorevaAnna <grigoreva.a.d.2004@gmail.com>
2026-01-18 15:45:34 +0200

    correction

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="booktracker-backend",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi==0.104.1",
+        "uvicorn[standard]==0.24.0",
+        "pydantic==2.5.0",
+    ],
+)
